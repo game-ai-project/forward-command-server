7eb07de817684e8a2d3fba4adfff52e319371e58
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-01-12 10:30:32 +0900

    Create script.py

---
+++ script.py
@@ -0,0 +1,29 @@
+import pyautogui
+import requests
+import schedule
+import time
+
+def job():
+    url = "https://28fe-133-19-43-10.jp.ngrok.io/poll"
+    response = requests.request("GET", url)
+    cheer = response.json()['cheer']
+    jeer = response.json()['jeer']
+    print(f'{time.localtime().tm_year}/{time.localtime().tm_mon}/{time.localtime().tm_mday} {time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}', ' - cheer - ', cheer, ' - jeer - ', jeer)
+    
+    if jeer > cheer:
+         # S Key -> Monsters move faster x2
+        pyautogui.moveTo(30, 30)
+        pyautogui.click()
+        pyautogui.press('S')
+    else:
+        # R Key -> Change all monsters to be eatable
+        pyautogui.moveTo(30, 30)
+        pyautogui.click()
+        pyautogui.press('R')
+   
+schedule.every(30).seconds.do(job)
+
+while 1:
+    job()
+    schedule.run_pending()
+    time.sleep(29)
